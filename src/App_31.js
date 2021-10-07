830b606a335a184e4097692ab1a4cf39259e3611
209410231 <209410231@gms.tku.edu.tw>
2021-10-07 18:15:20 +0800

    first commit

---
+++ src/App_31.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo_31 from './components/Todo_31';
+
+function App_31() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_31 text="Learn React" />
+      <Todo_31 text="Learn HTML" />
+      <Todo_31 text="Learn Python" />
+    </div>
+  );
+}
+
+export default App_31;
